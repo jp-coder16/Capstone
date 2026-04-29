283ee5e12a36bc84c1d74a72ef38fea9fd1df97f
JayantPachori-coder <jayantpachori2004@gmail.com>
2026-04-29 12:00:50 +0530

    6th commit

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -185,7 +185,7 @@ export default function LoginPage() {
                 color: 'var(--text-secondary)', fontSize: 13, cursor: 'pointer',
                 transition: 'var(--transition)'
               }}
-            >🔧 Admin </button>
+            >🔧 Admin click here </button>
           </div>
         </div>
 
